820f3ecb853449783841e74fcb450c156b2298b0
lap1sid <yukir0dev@gmail.com>
2020-01-04 18:44:06 +0900

    Added information data

---
+++ __init__.py
@@ -0,0 +1,10 @@
+import asyncio
+
+class library_info:
+    VERSION = "Alpha 0.1.0"
+    AUTHOR = "SereneTeam"
+    API_VERSION = "Alpha 0.1"
+    
+print("Version: " + library_info.VERSION)
+print("Author: " + library_info.AUTHOR)
+print("API Version: " + library_info.API_VERSION)
