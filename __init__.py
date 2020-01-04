8cf18d155d57be0a8ce013c7e6a71a830599cfe9
lap1sid <yukir0dev@gmail.com>
2020-01-05 08:09:16 +0900

    Added some codes

--- __init__.py
+++ __init__.py
@@ -1,8 +1,15 @@
+###                 GNU LESSER GENERAL PUBLIC LICENSE
+###                       Version 3, 29 June 2007
+### Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+### Everyone is permitted to copy and distribute verbatim copies
+### of this license document, but changing it is not allowed.
+
 # Modules importing
-#from .marker import colors
+#from libserenepy import marker
 #import asyncio
 
 class library_info:
+    LIBRARY_NAME = "libserenepy"
     BRANCH_VERSION = "Alpha"
     VERSION = "0.1.0"
     AUTHOR = "SereneTeam"
@@ -10,6 +17,7 @@ class library_info:
 
 # Testing Code
 if __name__ == "__main__":
-    print("Version: " + library_info.VERSION)
+    print("Library name: " + library_info.LIBRARY_NAME)
+    print("Version: " + library_info.BRANCH_VERSION + " " +library_info.VERSION)
     print("Author: " + library_info.AUTHOR)
     print("API Version: " + library_info.API_VERSION)
